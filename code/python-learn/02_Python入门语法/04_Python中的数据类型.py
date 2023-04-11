e4345c0afa5e2d663ca4bc1ae72da97c3e63d5a4
wenbin <wenbin@chinasofti.com>
2023-04-11 17:17:34 +0800

    变量 type

--- code/python-learn/02_Python入门语法/04_Python中的数据类型.py
+++ code/python-learn/02_Python入门语法/04_Python中的数据类型.py
@@ -2,7 +2,8 @@
 print(type("黑马程序员"))
 print(type(666))
 print(type(11.345))
-
+print(type({1, 2, 3}))
+print(type({1: 2}))
 
 # 方式2： 使用变量存储type()语句的结果
 string_type = type("黑马程序员")
@@ -11,7 +12,6 @@ float_type = type(11.345)
 print(string_type)
 print(int_type)
 print(float_type)
-
 
 # 方式3： 使用type()语句，查看变量中存储的数据类型信息
 name = "黑马程序员"
