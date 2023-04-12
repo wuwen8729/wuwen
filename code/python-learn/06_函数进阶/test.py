c71f5c4cb2d6683ff086b310f6381184b06095e9
wenbin <wenbin@chinasofti.com>
2023-04-12 17:51:25 +0800

    初始化

--- code/python-learn/06_函数进阶/test.py
+++ code/python-learn/06_函数进阶/test.py
@@ -5,4 +5,10 @@ def compute(add):
 def add(x, y):
     print(x + y)
 
-compute(lambda x, y: print(x + y))
+def devo(x,y):
+    print(x-y)
+def compute2(devo):
+    devo(3,1)
+
+# compute(lambda x, y: print(x + y))
+compute2(lambda x, y: print(x - y))
